ff9c6a7d6b86bc1a3b1606cceba5c37d113d02a3
Styrle <jserpis@hotmail.co.uk>
2020-10-09 21:30:15 +0100

    Adding layout design

--- weeks/1/week1.js
+++ weeks/1/week1.js
@@ -1,23 +1,25 @@
 function setup() {
   pixelDensity(5);
-  createCanvas(windowWidth, 800, WEBGL);
+  createCanvas(window.innerWidth, 800, WEBGL);
 
 }
 
 function draw() {
   background(0);
-  rotateX(frameCount * 0.05);
-  rotateY(frameCount * 0.05);
+  pop();
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
   noFill();
   stroke(100, 100, 240);
   strokeWeight(5); // Thicker
   box(400);
+  push();
 
   for(let i=0; i<5; i++){                 //counts through loop from 0 to 5
     stroke(100);
     box(30, 30, 30, 4, 4);
-    rotateX(frameCount * 0.05);
-    rotateY(frameCount * 0.05);
+    rotateX(frameCount * 0.02);
+    rotateY(frameCount * 0.02);
    }
 
 
